d7f671b0cb9c494f2db5b515dcdf1b0b904569ff
songwonseock <tablier@gmail.com>
2022-01-24 05:59:03 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,20 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// Unable to find an element with the text: /learn react/i. This could be because the text is broken up by multiple elements. In this case, you can provide a function for your text matcher to make your m
+// atcher more flexible.
